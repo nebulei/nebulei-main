77c1e5329945d1eea3c8e6e0bd3abec2e6a330b4
Adrian <adrian@adrianblanc.live>
2022-09-28 13:49:15 -0500

    adding margin to sides of div

--- src/containers/Home/partials/Hero.jsx
+++ src/containers/Home/partials/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
 
   return (
     <>
-    <section className=''>
+    <section className='mx-4'>
       <div className='bg-transparent'>
         <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
           <h1 className='text-black md:text-7xl sm:text-6xl text-4xl font-bold md:py-6 nebulei-font'>
