90e49b92b1ab524d4da1ca2337782f47a51b9b21
Adrian <adrian@adrianblanc.live>
2022-09-29 12:56:46 -0500

    added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,29 @@
 import React from 'react';
+import { Route, Routes } from "react-router-dom"
 
-import Navbar from  './components/Navbar/index'
-import Home from './containers/Home/index';
-import Footer from './components/Footer/Footer';
+import * as ROUTES from './constants/routes';
+
+import Navbar from './components/Navbar/index'
+import Home from './containers/Home';
+import NotFound from './containers/Core/404'
+import Footer from './components/Footer/index'
+
 
 
 function App() {
   return (
-      <>
-      <div id={`App`}>
+    <div id='App'>
 
       <Navbar />
-        <Home />
+
+      <Routes>
+          <Route path={ROUTES.HOME} element={<Home />} />
+          <Route path="*" element={<NotFound />} />
+      </Routes>
+
       <Footer />
-      
-      </div>
-      </>
+
+    </div>
   );
 }
 
